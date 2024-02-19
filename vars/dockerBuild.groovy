8191dd86e787c543b83682a68ae6c95f71fd897e
eswari <eswariganesula@gmail.com>
2024-02-19 14:26:06 +0530

    modified dockerBuild.groovy file

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,5 +1,5 @@
 def call(String userName, String imageName){
-    sh 'docker build --build-arg -t ${imageName} .'
+    sh 'docker build -t ${imageName} .'
     sh 'docker tag ${imageName} ${userName}/${imageName}:latest'
     withDockerRegistry([url:'https://index.docker.io/v1/', credentialsId:'docker']){
         sh 'docker push ${userName}/${imageName}:latest'
